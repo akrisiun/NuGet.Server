8cc041fbc768b9875725c257329346908c9360c3
AndriusK <andrius.k@sanitex.eu>
2017-04-07 22:10:25 +0300

    Include build/

---
+++ build/init.ps1
@@ -0,0 +1,58 @@
+[CmdletBinding()]
+param(
+    [string]$BuildBranch
+)
+
+# This file is downloaded to "build/init.ps1" so use the parent folder as the root
+$NuGetClientRoot = Split-Path -Path $PSScriptRoot -Parent
+
+Function Get-BuildTools {
+    param(
+        [string]$Branch
+    )
+
+    # Download common.ps1 and other tools used by this build script
+    $RootGitHubApiUri = "https://api.github.com/repos/NuGet/ServerCommon/contents"
+
+    if ($Branch) {
+        $Ref = '?ref=' + $Branch
+    } else {
+        $Ref = ''
+    }
+
+    Function Get-Folder {
+        [CmdletBinding()]
+        param(
+            [string]$Path
+        )
+        
+        $DirectoryPath = (Join-Path $NuGetClientRoot $FilePath)
+        if (-not (Test-Path $DirectoryPath)) {
+            New-Item -Path $DirectoryPath -ItemType "directory"
+        }
+        
+        $FolderUri = "$RootGitHubApiUri/$Path$Ref"
+        Write-Host "Downloading files from $FolderUri"
+        $Files = wget -UseBasicParsing $FolderUri | ConvertFrom-Json
+        Foreach ($File in $Files) {
+            $FilePath = $File.path
+            if ($File.type -eq "file") {
+                $DownloadUrl = $File.download_url
+                Write-Host "Downloading file at $DownloadUrl"
+                wget -UseBasicParsing -Uri $DownloadUrl -OutFile (Join-Path $NuGetClientRoot $FilePath)
+            } elseif ($File.type -eq "dir") {
+                Get-Folder -Path $FilePath
+            }
+        }
+    }
+
+    $FoldersToDownload = "build", "tools"
+    foreach ($Folder in $FoldersToDownload) {
+        Get-Folder -Path $Folder
+    }
+}
+
+Get-BuildTools -Branch $BuildBranch
+
+# Run common.ps1
+. "$NuGetClientRoot\build\common.ps1"
